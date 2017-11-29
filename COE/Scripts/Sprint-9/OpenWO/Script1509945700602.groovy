26e263cddea8019d26acf5d043f4d651d42cd8a6
ranjs1 <sumit.ranjan@in.pega.com>
2017-11-29 14:45:21 +0530

    test

--- COE/Scripts/Sprint-9/OpenWO/Script1509945700602.groovy
+++ COE/Scripts/Sprint-9/OpenWO/Script1509945700602.groovy
@@ -20,5 +20,8 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+
+
+//test
 WebUI.takeScreenshot()
 
